cbc1a2009f5302086a347433f906aefe35b5ccaa
m01x <fortiz.informatica@gmail.com>
2024-11-26 15:02:50 -0300

    feat( Rutas ): se definen rutas, endpoints y ya se cuenta con respuestas desde postman

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const express = require('express');
+require('dotenv').config();
 
 //Crear el servidor de Express
 const app = express();
 
 
+
+//Directorio publico
+app.use( express.static('public') );
+
+
+//Lectura y parseo del body
+app.use( express.json()); //middleware
+
+
 //Rutas
-app.get('/', ( req, res ) => {
+/**
+ * TODO: auth / Crear / Login / Renew
+ * TODO: CRUD evento
+ */
 
-  console.log('se requiere el /');
-  
-  res.json({
-    ok: true
-  });
-
-});
-
+app.use('/api/auth', require('./routes/auth'));
 
 //Escuchar peticiones
-app.listen( 4001, () => {
+app.listen( process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${4001}`);
 } );
 
