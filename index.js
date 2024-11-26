ee4d4c89d7892441e80ac8545cf778313e7f9865
m01x <fortiz.informatica@gmail.com>
2024-11-26 08:33:04 -0300

    feat( express ): Se instala express y se configura el listen en el puerto 4001

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hola mundoww');
+const express = require('express');
+
+//Crear el servidor de Express
+const app = express();
+
+
+//Rutas
+app.get('/', ( req, res ) => {
+
+  console.log('se requiere el /');
+  
+  res.json({
+    ok: true
+  });
+
+});
+
+
+//Escuchar peticiones
+app.listen( 4001, () => {
+  console.log(`Servidor corriendo en puerto ${4001}`);
+} );
+
